d89bdff4078f77edb276cf54016a95ede91acb40
Muriz Serifovic <muriz.se@gmail.com>
2020-11-08 22:58:31 +0100

    Finnish solve.py for problem 60

--- 60/solve.py
+++ 60/solve.py
@@ -11,10 +11,10 @@ def prime_gen(n):
                 primes[j] = False
     return primes
 
-C = prime_gen(int(6e4))
+C = prime_gen(int(3e4))
 P = [i for i in range(3, len(C)) if C[i]]
 
-BIG_C = prime_gen(int(1e7))
+BIG_C = prime_gen(int(1e8))
 BIG_P = [i for i in range(3, len(BIG_C)) if BIG_C[i]]
 
 PS = set(BIG_P)
@@ -26,9 +26,9 @@ def in_primes(*args) -> bool:
 def go_over(arr):
     new_arr = []
     for c in tqdm(arr):
-        for i in range(1, len(BIG_P)):
-            if BIG_P[i] not in c:
-                merged = set(list(c)+[BIG_P[i]])
+        for i in range(1, len(P)):
+            if P[i] not in c:
+                merged = set(list(c)+[P[i]])
                 if in_primes(*merged):
                     new_arr.append(set(merged))
     return new_arr
@@ -49,8 +49,10 @@ def sol(ps : int = 5):
         print(i)
         arr = go_over(old_arr)
         old_arr = arr[:]
-
-    return arr
+	min_s = float("inf")
+	for t in arr:
+		min_s = min(min_s, sum(t))
+    return min_s
 
 print(sol())
 
